b065a9966063c290b5c5167b9eaa7b8136ab3613
b-glitch <krangel@email.arizona.edu>
2021-09-20 15:54:09 -0700

    Added MainUI class.

---
+++ src/main/java/csc/arizona/moneymanager/MainUI.java
@@ -0,0 +1,11 @@
+package csc.arizona.moneymanager;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class MainUI extends Scene {
+
+    public MainUI(Parent parent) {
+        super(parent);
+    }
+}
