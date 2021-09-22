16ee81ce36eafed66475f61189b6de7aefae43f7
Carter Boyd <cboyd899@gmail.com>
2021-09-22 13:34:48 -0700

    AccountList.java methods added

--- src/main/java/csc/arizona/moneymanager/AccountList.java
+++ src/main/java/csc/arizona/moneymanager/AccountList.java
@@ -1,4 +1,23 @@
 package csc.arizona.moneymanager;
 
+import java.util.HashSet;
+
 public class AccountList {
+
+    private final HashSet<Account> accountList;
+
+    public AccountList() {
+        accountList = new HashSet<>();
+    }
+
+    public Account getAccount(String username) {
+        for (Account account : accountList)
+            if (account.getUsername().equals(username))
+                return account;
+        return null;
+    }
+
+    public void addAccount(Account user) {
+        accountList.add(user);
+    }
 }
