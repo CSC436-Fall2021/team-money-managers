5ee471ff3752727c6b36641a3461abb130dcaa34
b-glitch <krangel@email.arizona.edu>
2021-09-22 15:19:06 -0700

    Extracted menu bar to separate class, MainMenuBar. This new class encapsulates the menu items and on action events.

--- src/main/java/csc/arizona/moneymanager/MainUI.java
+++ src/main/java/csc/arizona/moneymanager/MainUI.java
@@ -52,6 +52,7 @@ public class MainUI {
     private void initializeScene(int width, int height){
 
         // Initializing elements
+        MainMenuBar menuBar = new MainMenuBar();
         initTransactionPane();
         initServicesPane();
         initOptionsPane();
@@ -67,7 +68,7 @@ public class MainUI {
         // Setting up main pane
         BorderPane mainPane = new BorderPane();
         BorderPane centerPane = new BorderPane();
-        mainPane.setTop(getMenuBar());
+        mainPane.setTop(menuBar);
         centerPane.setTop(transactionPane);
         centerPane.setCenter(servicesPane);
         mainPane.setCenter(centerPane);
@@ -75,56 +76,6 @@ public class MainUI {
 
         // creating scene
         this.scene = new Scene(mainPane, width, height);
-    }
-
-    /**
-     * Sets up menu options to a MenuBar and returns it.
-     * Each menu item calls a separate method as the OnAction. This
-     * isolates the action portion from the setup code.
-     * @return the initialized MenuBar object.
-     */
-    public MenuBar getMenuBar(){
-        MenuBar menu = new MenuBar();
-
-        // File menu setup
-        Menu file = new Menu("_File");
-        SeparatorMenuItem fileMenuSeparator = new SeparatorMenuItem();
-        // --> Save Data option
-        MenuItem saveData = new MenuItem("_Save Data");
-        saveData.setOnAction(e-> saveDataMenuAction() );
-        // --> Logout option
-        MenuItem logout = new MenuItem("_Logout");
-        logout.setOnAction(e-> logoutMenuAction() );
-        // Adding items to File menu
-        file.getItems().addAll(saveData, fileMenuSeparator, logout);
-
-        // Report menu setup
-        Menu reports = new Menu("_Reports");
-        // --> Show Report option //TODO maybe make showReport a submenu with types of reports as menu items
-        MenuItem showReport = new MenuItem("Show _Report");
-        showReport.setOnAction(e-> showReportsMenuAction() );
-        // --> Save... (report) option
-        MenuItem saveReport = new MenuItem("_Save...");
-        saveReport.setOnAction(e-> saveReportMenuAction() );
-        // Adding items to Reports menu
-        reports.getItems().addAll(showReport, saveReport);
-
-        // Help menu setup
-        Menu help = new Menu("_Help");
-        SeparatorMenuItem helpMenuSeparator = new SeparatorMenuItem();
-        // --> Help option
-        MenuItem showHelp = new MenuItem("_Help");
-        showHelp.setOnAction(e-> showHelpMenuAction() );
-        // --> About option
-        MenuItem about = new MenuItem("_About");
-        about.setOnAction(e-> aboutMenuAction() );
-        // Adding items to Help menu
-        help.getItems().addAll(showHelp, helpMenuSeparator, about);
-
-        // Adding menus to menu bar
-        menu.getMenus().addAll(file, reports, help);
-
-        return menu;
     }
 
     /**
@@ -169,52 +120,6 @@ public class MainUI {
      */
     public Scene getScene(){
         return this.scene;
-    }
-
-    /* **********************************
-     * Event Action Methods
-     ************************************/
-
-    /**
-     * Contains the actions performed when the Menu option "Save Data" is selected.
-     */
-    private void saveDataMenuAction(){
-        System.out.println("Save Data Selected");
-    }
-
-    /**
-     * Contains the actions performed when the Menu option "Logout" is selected.
-     */
-    private void logoutMenuAction(){
-        System.out.println("Logout Selected");
-    }
-
-    /**
-     * Contains the actions performed when the Menu option "Show Report" is selected.
-     */
-    private void showReportsMenuAction(){
-        System.out.println("Show Report selected");
-    }
-
-    /**
-     * Contains the actions performed when the Menu option "Save..."(Report)  is selected.
-     */
-    private void saveReportMenuAction(){
-        System.out.println("Save Report selected");
-    }
-
-    /**
-     * Contains the actions performed when the Menu option "Help" is selected.
-     */
-    private void showHelpMenuAction(){
-        System.out.println("Show Help selected");
-    }
-
-    /**
-     * Contains the actions performed when the Menu option "About" is selected.
-     */
-    private void aboutMenuAction(){
-        System.out.println("About selected");
     }
 
 }
