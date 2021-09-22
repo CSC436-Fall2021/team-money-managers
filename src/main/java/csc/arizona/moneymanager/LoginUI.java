7bc4c37857a4ac0f4f72dd3c4f78a9caa2465d09
Carter Boyd <cboyd899@gmail.com>
2021-09-22 12:08:14 -0700

    Created Text & TextFields to login

--- src/main/java/csc/arizona/moneymanager/LoginUI.java
+++ src/main/java/csc/arizona/moneymanager/LoginUI.java
@@ -2,7 +2,10 @@ package csc.arizona.moneymanager;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class LoginUI extends Application {
@@ -14,11 +17,27 @@ public class LoginUI extends Application {
     @Override
     public void start(Stage primaryStage) {
         BorderPane pane = new BorderPane();
+        pane.setCenter(createLogin());
         Scene scene = new Scene(pane, 600, 500);
         primaryStage.setTitle("Money Manager");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-
+    /**
+     * @return the text and text fields necessary for the user to login in to the money management
+     * site
+     */
+    private static VBox createLogin() {
+        VBox loginTextFields = new VBox();
+        Text login = new Text("Username");
+        TextField loginField = new TextField();
+        loginField.setMaxWidth(150);
+        Text password = new Text("Password");
+        TextField passwordField = new TextField();
+        passwordField.setMaxWidth(150);
+        loginTextFields.getChildren().addAll(new Text("Welcome"), login, loginField, password,
+                passwordField);
+        return loginTextFields;
+    }
 }
