65cdf11587a42a55608fad403b100aca685bd35e
masonbaier <masonbaier@email.arizona.edu>
2021-09-25 00:02:42 -0700

    Added logic of adding users, validating users, deleting users, and checking if a user exists by connected to the database setup. Added unit tests for those features. Yet to finish adding a custom object as a value in the data table.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,15 @@
 module csc.arizona.moneymanager {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.mongodb.driver.core;
+    requires org.mongodb.driver.sync.client;
+    requires junit;
+    requires org.mongodb.bson;
+    requires com.google.gson;
+    requires jdk.jsobject;
 
 
     opens csc.arizona.moneymanager to javafx.fxml;
-    exports csc.arizona.moneymanager;
+    exports csc.arizona.moneymanager.database;
+    opens csc.arizona.moneymanager.database to javafx.fxml;
 }
