375e49238e532641100a384eefc7870d96c65005
Carter Boyd <cboyd899@gmail.com>
2021-10-11 11:54:34 -0700

    created UserSetting.java as well as created skeleton of all methods the username may need

---
+++ src/main/java/csc/arizona/moneymanager/MainUI/UserSetting.java
@@ -0,0 +1,104 @@
+package csc.arizona.moneymanager.MainUI;
+
+import java.util.ArrayList;
+
+/**
+ * @author Carter Boyd
+ *
+ * This class is designed to hold the settings of what the specific user would like to have
+ * specified to his account settings specifically what kind of custom categories the user may
+ * want as well as how long the user would want his budget stored and how much of a budget they
+ * would like
+ *
+ * @implNote data will be reached from the database so the database will have to have these
+ * structures and store them
+ */
+public class UserSetting {
+
+    private int budget;
+    private ArrayList<String> customCategory;
+    private String budgetDuration;
+
+    /**
+     * default constructor for if this is the first time an account has been created and will
+     * have to fill out the desired amount
+     */
+    public UserSetting() {
+
+    }
+
+    /**
+     * constructor for when the user has already made an account and needs the data retreived
+     * from the database
+     * @param category the custom category list
+     */
+    public UserSetting(ArrayList<String> category) {
+
+    }
+
+    /**
+     * @return the budget
+     */
+    public int getBudget() {
+        return budget;
+    }
+
+    /**
+     * sets the budget that the user wants
+     * @param budget the desired amount
+     */
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+    /**
+     * @return the list of categories from the custom category list
+     */
+    public ArrayList<String> getCustomCategory() {
+        return customCategory;
+    }
+
+    /**
+     * sets the custom amount array list
+     * @param customCategory the custom category list that was found in the database
+     */
+    public void setCustomCategory(ArrayList<String> customCategory) {
+        this.customCategory = customCategory;
+    }
+
+    /**
+     * @return how long the user wants the transferred data stored
+     */
+    public String getBudgetDuration() {
+        return budgetDuration;
+    }
+
+    /**
+     * user wants to change how long their budget is stored
+     * @param budgetDuration the new amount of time they want their transfers stored in the database
+     */
+    public void setBudgetDuration(String budgetDuration) {
+        this.budgetDuration = budgetDuration;
+    }
+
+    /**
+     * if the user created a category name and wants to rename it
+     */
+    public void editCategoryName() {
+
+    }
+
+    /**
+     * removes a custom category from the category list
+     */
+    public void removeCategoryName() {
+
+    }
+
+    /**
+     * adds the custom category to the category list
+     */
+    public void addCategoryName() {
+
+    }
+}
