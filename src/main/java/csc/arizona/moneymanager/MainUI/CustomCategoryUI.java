c8f239fc65a130c3e57985979b7d1d0007ae4d3d
Joseph <clancy1@email.arizona.edu>
2021-10-18 03:16:04 -0700

    Reimplemented CustomCategoryUI and made actions go to the Options pane.

--- src/main/java/csc/arizona/moneymanager/MainUI/CustomCategoryUI.java
+++ src/main/java/csc/arizona/moneymanager/MainUI/CustomCategoryUI.java
@@ -2,38 +2,35 @@ package csc.arizona.moneymanager.MainUI;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
 import java.util.List;
 
-public class CustomCategoryUI extends HBox {
-
-    List<String> dropDownCategories;
-    List<String> userCategories;
+public class CustomCategoryUI extends ServicesView {
 
     TextField userInput;
-    Button enterButton;
 
-    public CustomCategoryUI(MainUI callback, List<String> addTo) {
-        userCategories = addTo;
+    public CustomCategoryUI() {
+        super("Add Category", "Cancel");
 
-        userInput = new TextField();
-        userInput.setPromptText("Enter new category");
-
-        enterButton = new Button("Enter");
-
-        userInput.setOnAction(new EnterCategoryHandler());
-        enterButton.setOnAction(new EnterCategoryHandler());
 
 
     }
 
-    private class EnterCategoryHandler implements EventHandler<ActionEvent> {
-        @Override
-        public void handle(ActionEvent actionEvent) {
+    @Override
+    void initContent() {
+        userInput = new TextField();
+        userInput.setPromptText("Enter new category");
 
-        }
+        content.addRow(14, userInput);
+
+        content.setAlignment(Pos.CENTER);
+    }
+
+    public String getInput() {
+        return userInput.getText();
     }
 }
