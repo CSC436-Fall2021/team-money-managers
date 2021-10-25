5f7d9fbdb4ee9a01df76535e3760809e819783df
Joseph <clancy1@email.arizona.edu>
2021-10-24 19:42:13 -0700

    Fixed imports.

--- src/main/java/csc/arizona/moneymanager/Charts/Histogram.java
+++ src/main/java/csc/arizona/moneymanager/Charts/Histogram.java
@@ -1,5 +1,8 @@
 package csc.arizona.moneymanager.Charts;
 
+import csc.arizona.moneymanager.TransactionUI.Transaction;
+import java.util.List;
+
 public class Histogram extends Chart {
 
     public Histogram(List<Transaction> transactions) {
--- src/main/java/csc/arizona/moneymanager/Charts/ScatterChart.java
+++ src/main/java/csc/arizona/moneymanager/Charts/ScatterChart.java
@@ -1,5 +1,8 @@
 package csc.arizona.moneymanager.Charts;
 
+import csc.arizona.moneymanager.TransactionUI.Transaction;
+import java.util.List;
+
 public class ScatterChart extends Chart {
 
     public ScatterChart(List<Transaction> transactions) {
