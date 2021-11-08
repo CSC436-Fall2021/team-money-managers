4b6c6e3d3f2903395cdf8ba77543953aebb10b6a
Joseph <clancy1@email.arizona.edu>
2021-11-07 21:41:28 -0700

    PieView mostly done. Will finish implementing displays for charts after merge. (so I can modify necessary MainUI methods that are not on this branch.

--- src/main/java/csc/arizona/moneymanager/Charts/PieView.java
+++ src/main/java/csc/arizona/moneymanager/Charts/PieView.java
@@ -14,16 +14,35 @@ public class PieView extends Chart {
 
         Set<String> categoryNames = data.getCategorySet();
 
-        List<PieChart.Data> piDataIncome = new ArrayList<>(); // pi data for for income data pi chart
-        List<PieChart.Data> piDataExpense = new ArrayList<>(); // pi data for expense data pi chart
+        List<PieChart.Data> pieDataIncome = new ArrayList<>(); // pi data for for income data pi chart
+        List<PieChart.Data> pieDataExpense = new ArrayList<>(); // pi data for expense data pi chart
 
+        // set up PieChart.Data objects for Java's PieChart
+        for (String category : categoryNames) {
+            // getGross functions return 0 if N/A for category.
+            double categoryIncome = data.getGrossIncomeCategory(category);
+            double categoryExpense = data.getGrossIncomeCategory(category);
 
+            // if there was income for a category, add it to the income pie chart
+            if (categoryIncome != 0) {
+                PieChart.Data pieData = new PieChart.Data(category, categoryIncome);
+                pieDataIncome.add(pieData);
+            }
 
-        ObservableList<PieChart.Data> piIncomeObservable = FXCollections.observableArrayList(piDataIncome);
-        ObservableList<PieChart.Data> piExpenseObservable = FXCollections.observableArrayList(piDataExpense);
+            // if there was expense for a category, add it to the expense pie chart
+            if (categoryExpense != 0) {
+                PieChart.Data pieData = new PieChart.Data(category, categoryExpense);
+                pieDataExpense.add(pieData);
+            }
+        }
 
-        PieChart incomeChart = new PieChart(piIncomeObservable);
-        PieChart expenseChart = new PieChart(piExpenseObservable);
+        // set up actual PieCharts using the PieChart.Data objects we created
+        ObservableList<PieChart.Data> pieIncomeObservable = FXCollections.observableArrayList(pieDataIncome);
+        ObservableList<PieChart.Data> pieExpenseObservable = FXCollections.observableArrayList(pieDataExpense);
 
+        PieChart incomeChart = new PieChart(pieIncomeObservable);
+        PieChart expenseChart = new PieChart(pieExpenseObservable);
+
+        // display
     }
 }
