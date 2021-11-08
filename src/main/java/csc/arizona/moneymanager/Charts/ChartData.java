b6f5c88a633e240fb0e869a22a2e057785b2571f
Joseph <clancy1@email.arizona.edu>
2021-11-07 21:32:24 -0700

    Added functionality to ChartData and refactored PieView.

--- src/main/java/csc/arizona/moneymanager/Charts/ChartData.java
+++ src/main/java/csc/arizona/moneymanager/Charts/ChartData.java
@@ -2,18 +2,31 @@ package csc.arizona.moneymanager.Charts;
 
 import csc.arizona.moneymanager.TransactionUI.Transaction;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+/**
+ * Provides useful functions for getting data about a list of Transactions
+ *  for the purpose of displaying graphical information in a chart.
+ */
 public class ChartData {
+
+    //private Set<String> categoryNames; // ease of access color coding. just a set of all transaction categories.
+    // not needed because == categorySums.keySet().
 
     private Map<String, List<Transaction>> positiveBalances;
     private Map<String, List<Transaction>> negativeBalances;
+    private Map<String, Double> categorySums;
 
     public ChartData(List<Transaction> transactions) {
+        //categoryNames = new HashSet<String>();
+        positiveBalances = new HashMap<>();
+        negativeBalances = new HashMap<>();
+        categorySums = new HashMap<>();
+
         for (Transaction transaction : transactions) {
             String category = transaction.getCategory();
+
+            //categoryNames.add(category);
 
             if (transaction.isIncome()) {
                 if (!positiveBalances.containsKey(category)) {
@@ -29,31 +42,84 @@ public class ChartData {
                 negativeBalances.get(category).add(transaction);
             }
 
+            if (!categorySums.containsKey(category)) {
+                categorySums.put(category, transaction.getAmount());
+            } else {
+                double prevAmount = categorySums.get(category);
+                categorySums.put(category, prevAmount + transaction.getAmount());
+            }
 
         }
     }
 
+    /**
+     * Returns the list of all negative balances of a category's spending.
+     * @param category
+     * @return list of all negative balances of a category's spending.
+     */
     public List<Transaction> getExpensesCategory(String category) {
         if (negativeBalances.containsKey(category)) {
             return negativeBalances.get(category);
         }
 
-        return null;
+        return new ArrayList<Transaction>(); // no null check needed in caller
     }
 
-    public int getExpensesCategorySum(String category) {
-        return 0;
+    /**
+     * Returns the gross expense (all negative balances) of a category's spending.
+     * @param category
+     * @return sum of all negative balances of a category's spending.
+     */
+    public double getGrossExpenseCategory(String category) {
+        double sum = 0.0;
+
+        for (Transaction transaction : negativeBalances.get(category)) {
+            sum += transaction.getAmount();
+        }
+        return sum;
     }
 
+    /**
+     * Returns the list of all positive balances of a category's spending.
+     * @param category
+     * @return list of all positive balances of a category's spending.
+     */
     public List<Transaction> getIncomesCategory(String category) {
         if (positiveBalances.containsKey(category)) {
             return positiveBalances.get(category);
         }
 
-        return null;
+        return new ArrayList<Transaction>(); // no null check needed in caller
     }
 
-    public int getIncomesCategorySum(String category) {
-        return 0;
+    /**
+     * Returns the gross income (all positive balances) of a category's spending.
+     * @param category
+     * @return sum of all positive balances of a category's spending.
+     */
+    public double getGrossIncomeCategory(String category) {
+        double sum = 0.0;
+
+        for (Transaction transaction : positiveBalances.get(category)) {
+            sum += transaction.getAmount();
+        }
+        return sum;
+    }
+
+    /**
+     * Returns the net sum of incomes and expenses of a category's spending.
+     * @param category
+     * @return net sum of category's spending.
+     */
+    public double getNetCategory(String category) {
+        return categorySums.get(category);
+    }
+
+    /**
+        Returns the set of categories of the transactions the ChartData was constructed with.
+     @return set of categories.
+     */
+    public Set<String> getCategorySet() {
+        return categorySums.keySet();
     }
 }
--- src/main/java/csc/arizona/moneymanager/Charts/PieView.java
+++ src/main/java/csc/arizona/moneymanager/Charts/PieView.java
@@ -5,35 +5,25 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.chart.PieChart;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PieView extends Chart {
 
     public PieView(List<Transaction> transactions) {
-        List<PieChart.Data> test = new ArrayList<>();
-        Map<String, Double> sums = new HashMap<>();
+        data = new ChartData(transactions);
 
-        for (Transaction transaction : transactions) {
-            String category = transaction.getCategory();
+        Set<String> categoryNames = data.getCategorySet();
 
-            if (!sums.containsKey(category)) {
-                sums.put(category, transaction.getAmount());
-            } else {
-                sums.put(category, sums.get(category) + transaction.getAmount());
-            }
+        List<PieChart.Data> piDataIncome = new ArrayList<>(); // pi data for for income data pi chart
+        List<PieChart.Data> piDataExpense = new ArrayList<>(); // pi data for expense data pi chart
 
-        }
 
-        for (String key : sums.keySet()) {
-            test.add(new PieChart.Data(key, sums.get(key)));
-        }
-        ObservableList<PieChart.Data> data = FXCollections.observableArrayList(test);
 
-        PieChart chart = new PieChart(data);
+        ObservableList<PieChart.Data> piIncomeObservable = FXCollections.observableArrayList(piDataIncome);
+        ObservableList<PieChart.Data> piExpenseObservable = FXCollections.observableArrayList(piDataExpense);
 
-        //data = new ChartData(transactions);
+        PieChart incomeChart = new PieChart(piIncomeObservable);
+        PieChart expenseChart = new PieChart(piExpenseObservable);
+
     }
 }
