c437a46f443ac535ccb070e52274f6f2a1a2415c
masonbaier <60157941+masonbaier@users.noreply.github.com>
2021-11-21 15:54:11 -0700

    Revert "Scatter view, histogram color differentiation."

--- src/main/java/csc/arizona/moneymanager/Charts/TransactionChart.java
+++ src/main/java/csc/arizona/moneymanager/Charts/TransactionChart.java
@@ -1,15 +1,12 @@
 package csc.arizona.moneymanager.Charts;
 
-import javafx.scene.control.Label;
+import csc.arizona.moneymanager.MainUI.ServicesView;
 import javafx.scene.layout.Pane;
 
 public abstract class TransactionChart {
 
-    protected static final Label MISSING_DATA_LABEL = new Label("No Transaction data available.");
-
     protected String title;
     protected ChartData data;
-    //protected Chart mainChart; // not in use for now.
 
     public String getTitle() {
         return title;
